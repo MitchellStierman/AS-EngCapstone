0d566072fdf584dd26dcd171969aa67a4a6e8378
Mitchell <stiermanm@gmail.com>
2023-10-14 17:11:24 -0600

    Initial Commit, Fixes needed still.

---
+++ Tests/SQLiteTest/app/src/main/java/com/example/financialtracker/DatabaseHandler.java
@@ -0,0 +1,84 @@
+package com.example.financialtracker;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+public class DatabaseHandler extends SQLiteOpenHelper{
+    private static final String DB_NAME = "finance";
+    private static final int DB_VERSION = 1;
+    private static final String TABLE_NAME_INCOME = "income";
+    private static final String TABLE_NAME_EXPENSES = "expenses";
+    private static final String ID_COL = "id";
+    private static final String DESC_COL = "description";
+    private static final String DATE_COL = "date";
+    private static final String AMOUNT_COL = "amount";
+
+    public DatabaseHandler(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String incomeTableCreate = "CREATE TABLE " + TABLE_NAME_EXPENSES + " ("
+                + ID_COL + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + DESC_COL + " TEXT, "
+                + DATE_COL + " TEXT, "
+                + AMOUNT_COL + " INTEGER)";
+
+        String expensesTableCreate = "CREATE TABLE " + TABLE_NAME_INCOME + " ("
+                + ID_COL + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + DESC_COL + " TEXT, "
+                + DATE_COL + " TEXT, "
+                + AMOUNT_COL + " INTEGER)";
+
+        db.execSQL(incomeTableCreate);
+        db.execSQL(expensesTableCreate);
+    }
+
+    public void addNewIncome(String desc, Integer amount) {
+            SQLiteDatabase db = this.getWritableDatabase();
+            ContentValues values = new ContentValues();
+            TimeZone tz = TimeZone.getDefault();
+            Calendar calendar = new GregorianCalendar(tz);
+            String date = calendar.get(Calendar.MONTH) + "/" + calendar.get(Calendar.DATE)
+                    + "/" + calendar.get(Calendar.YEAR);
+
+            values.put(DESC_COL, desc);
+            values.put(DATE_COL, date);
+            values.put(AMOUNT_COL, amount);
+
+            db.insert(TABLE_NAME_INCOME, null, values);
+            db.close();
+
+    }
+
+    public void addNewExpense(String desc, Integer amount){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        TimeZone tz = TimeZone.getDefault();
+        Calendar calendar = new GregorianCalendar(tz);
+        String date = calendar.get(Calendar.MONTH) + "/" + calendar.get(Calendar.DATE)
+                + "/" + calendar.get(Calendar.YEAR);
+
+        values.put(DESC_COL, desc);
+        values.put(DATE_COL, date);
+        values.put(AMOUNT_COL, amount);
+
+        db.insert(TABLE_NAME_INCOME, null, values);
+        db.close();
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
---
+++ Tests/SQLiteTest/app/src/main/java/com/example/financialtracker/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.financialtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    int income, expense;
+    EditText incomeInput, expenseInput;
+
+    Button submitButton, submitButton2;
+
+    private DatabaseHandler db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        incomeInput = (EditText) findViewById(R.id.incomeInput);
+        submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (TextUtils.isEmpty(incomeInput.getText().toString())){
+                    //showToast("Please enter an amount.");
+                    incomeInput.setError("You must enter an amount.");
+                } else {
+                    income = Integer.parseInt(incomeInput.getText().toString());
+
+                    db = new DatabaseHandler(MainActivity.this);
+                    db.addNewIncome("test", income);
+                    showToast("Income Added");
+                }
+
+            }
+
+        });
+
+        expenseInput = (EditText) findViewById(R.id.expenseInput);
+        submitButton2 = (Button) findViewById(R.id.submitButton2);
+        submitButton2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (TextUtils.isEmpty(expenseInput.getText().toString())) {
+                    //showToast("Please enter an amount.");
+                    expenseInput.setError("You must enter an amount.");
+
+                } else {
+                    expense = Integer.parseInt(expenseInput.getText().toString());
+
+                    db = new DatabaseHandler(MainActivity.this);
+                    db.addNewIncome("test", expense);
+                    showToast("Expense Submitted");
+                }
+            }
+        });
+
+    }
+
+    private void showToast(String text){
+        Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+
+    }
+
+
+}
